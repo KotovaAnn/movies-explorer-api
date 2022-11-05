5d3d75a52e5f944ff965b3ec16522f2359c2ccf1
Kotova Anna <merpri@mail.ru>
2022-11-05 11:46:08 +0300

    fix: cors

--- controllers/users.js
+++ controllers/users.js
@@ -101,7 +101,7 @@ const login = async (req, res, next) => {
       {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        SameSite: 'None',
+        SameSite: 'none',
         Secure: true,
       },
     );
